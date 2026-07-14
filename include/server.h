67d47c64a84002923ee40c997dbf0ae9d566785e
signorepesce <171577805+signorepesce@users.noreply.github.com>
2026-07-14 20:18:17 +0300

    add server core interface

---
+++ include/server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#define SERVER_WORKERS    8
+#define SERVER_QUEUE      64
+#define SERVER_ARENA      (1024 * 1024)
+#define SERVER_REQ_MAX    (256 * 1024)
+#define SERVER_BODY_MAX   (256 * 1024)
+#define SERVER_TIMEOUT_S  5
+#define SERVER_DEADLINE_S 10
+#define SERVER_IDLE_S     5
+#define SERVER_KEEPALIVE_MAX 100
+
+int server_run(int port);
+
+#endif
